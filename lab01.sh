ddc3e6bf47ec5b396e96893a192bc0e1575ae348
milton01031997 <167126597+milton01031997@users.noreply.github.com>
2025-09-10 16:07:00 -0300

    Create lab01.sh

---
+++ lab01.sh
@@ -0,0 +1,28 @@
+sudo groupadd desenvolvedores
+sudo groupadd testadores
+
+sudo useradd -m -g desenvolvedores dev_senior
+sudo useradd -m -g desenvolvedores dev_junior
+sudo useradd -m -g testadores qa_analyst
+sudo usermod -aG testadores dev_senior
+
+sudo passwd <mouse>
+
+sudo mkdir -p /srv/projetos/backend
+sudo mkdir -p /srv/projetos/frontend
+
+sudo chown -R dev_senior:desenvolvedores /srv/projetos
+sudo chmod -R 750 /srv/projetos
+ls -l /srv/
+
+#!/bin/bash
+echo '=========================================='
+echo 'Bem-vindo(a) ao Servidor de Desenvolvimento!'
+echo "Data e hora atual: $(date)"
+echo '=========================================='
+Dê permissão de execução:
+sudo chmod 755 /usr/local/bin/boasvindas.sh
+Adicione a chamada do script no arquivo /etc/profile:
+/usr/local/bin/boasvindas.sh
+
+
